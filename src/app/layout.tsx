91a2c71825344433e7fb9fe232fec7703ba4900f
d-zucho <danny.nico.dev@outlook.com>
2025-01-18 15:35:25 -0800

    start header desktop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Jost } from 'next/font/google'
 
 import './globals.css'
+import Header from '@/components/Header/Header'
 
 const jost = Jost({
   variable: '--font-jost',
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${jost.className} text-black  bg-white antialiased`}>
+        <Header />
         {children}
       </body>
     </html>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import MaxWidthWrapper from '../MaxWidthWrapper'
+import Image from 'next/image'
+import { NAV_LINKS } from '@/constants'
+import Link from 'next/link'
 
 const Header = () => {
-  return <header>Header</header>
+  return (
+    <header className='w-full py-16'>
+      <MaxWidthWrapper className='flex justify-between items-center'>
+        <Image src={'/logo-dark.png'} alt='logo' width={200} height={25} />
+        <div className='flex items-center gap-8'>
+          {NAV_LINKS.map((link, index) => (
+            <Link key={link.label} href={link.href} className='tracking-[2px]'>
+              {link.label.toUpperCase()}
+            </Link>
+          ))}
+        </div>
+      </MaxWidthWrapper>
+    </header>
+  )
 }
 
 export default Header
