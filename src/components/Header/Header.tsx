72f4d870893b87698186e2ad6affedb4fd1207e3
d-zucho <danny.nico.dev@outlook.com>
2025-01-18 15:46:15 -0800

    start mobile nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
     <header className='w-full py-16'>
       <MaxWidthWrapper className='flex justify-between items-center'>
         <Image src={'/logo-dark.png'} alt='logo' width={200} height={25} />
-        <div className='flex items-center gap-8'>
+        <div className='flex items-center gap-10'>
           {NAV_LINKS.map((link, index) => (
             <Link key={link.label} href={link.href} className='tracking-[2px]'>
               {link.label.toUpperCase()}
