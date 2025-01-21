ff1a52d1c5b38663a018b08e1154243f918a61f7
d-zucho <danny.nico.dev@outlook.com>
2025-01-20 18:50:26 -0800

    finish responsive hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,14 @@ export default function Home() {
     <div>
       <p>testing this</p>
       <Hero />
+      <div className='w-full bg-black text-white mt-10'>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora quas
+          excepturi adipisci nobis, veniam dignissimos exercitationem, numquam
+          laboriosam necessitatibus maxime sit. Repellendus officia omnis a
+          fugit porro. Amet, repellendus ducimus.
+        </p>
+      </div>
     </div>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,53 @@
 import React from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import { buttonVariants } from './ui/button'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
     <section className='w-full'>
-      <MaxWidthWrapper className=''>
-        <div className='bg-peach'>
-          <div className='text-white'>
+      <MaxWidthWrapper className='z-0'>
+        <div className='bg-peach px-20 py-28 overflow-hidden relative flex justify-between flex-col lg:flex-row gap-20 rounded-lg'>
+          <div className='text-white  lg:max-w-lg text-center lg:text-left'>
             <h1 className='h1'>
               Award-winnding custom <br />
               designs and digital <br />
               branding solutions
             </h1>
-            <p>
+            <p className='leading-[26px] mb-10 max-w-sm lg:mx-0 mx-auto mt-4'>
               With over 10 years in the industry, we are experienced in creating
               fully responsive websites, app design, and engaging brand
               experiences. Find out more about our services.
             </p>
+            <Link
+              href='/'
+              className={cn(
+                buttonVariants({ variant: 'outline' }),
+                'text-dark-grey text-[15px] font-medium tracking-wider border-none hover:bg-light-peach hover:text-white p-6'
+              )}
+            >
+              LEARN MORE
+            </Link>
           </div>
-          <div></div>
+
+          <div className='md:-mb-80'>
+            <Image
+              src='/phone-image3.png'
+              alt=''
+              width={400}
+              height={550}
+              className='max-w-xs w-[300px] h-[550px] mx-auto lg:mx-0'
+            />
+            <Image
+              src={'/assets/home/desktop/bg-pattern-hero-home.svg'}
+              alt='hero'
+              width={650}
+              height={500}
+              className='absolute top-0 right-0'
+            />
+          </div>
         </div>
       </MaxWidthWrapper>
     </section>
